e475e13a5fb4eac3267fb912d5b3f02f60658947
Andmontc <oscarandresmontes@gmail.com>
2020-08-04 19:57:10 +0000

    puppet advanced

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,10 +1,9 @@
-#!/usr/bin/env bash
 #SSH configuration file so that you can connect to a server without typing a password
 
 file_line { 'Identityfile':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => 'Identityfile ~/.ssh/holberton',
+  line   => 'IdentityFile ~/.ssh/holberton',
 }
 
 file_line { ' password off':
